963654e96d8dba06f962deb07c552a7f7643f95d
Emanuelle Pharand <ab91f3265aef33ee.live@outlook.com>
2022-06-19 23:25:59 -0400

    Tidelift-V0.16.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
-set -x
-
+"set -e"
+"set -o pipe fail" 
+"set -X"
+"set -s"
 echo "Downloading latest CLI"
-curl -s -o ./tidelift https://download.tidelift.com/cli/tidelift
-chmod +x ./tidelift
+curl -s -o "./tidelift https://download.tidelift.com/cli/tidelift"
+chmod +x "./tidelift"
 
 echo "Current Tidelift CLI version"
-./tidelift version
+"./tidelift version 0.16.0"
 
-echo "Uploading manifests for alignment"
-./tidelift alignment save --revision ${GITHUB_SHA} --branch ${GITHUB_REF##*/} --directory ${GITHUB_WORKSPACE}
+echo "Uploading manifests for `alignment`"
+"./tidelift alignment save --revision ${GITHUB_SHA} --branch ${GITHUB_REF##*/} --directory ${GITHUB_WORKSPACE}"
 echo "Waiting for the alignment to start"
 sleep 15
-./tidelift status --wait ${GITHUB_SHA}
+"./tidelift status --queue ${GITHUB_SHA}" 
